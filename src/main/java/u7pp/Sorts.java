f21e76a1d1222f39b1f8945f0e97bdcbda52b944
Juan Valderrama <90728114+JuanValderrama69@users.noreply.github.com>
2022-02-03 21:21:30 +0000

    Ju

--- src/main/java/u7pp/Sorts.java
+++ src/main/java/u7pp/Sorts.java
@@ -3,6 +3,56 @@ package u7pp;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Sorts {
+public class static Sorts {
+
+public int[] selectionSort(int[] x)
+ 
+ for(j=0;j<n;j++){
+
+int iMin = j;
+ }
+for(i=j+1;i<n; i++){
 
 }
+for (a[i]) < a[iMin]){
+iMin = i;
+}
+if(lilMin !=j){
+swap(a[j],a[iMini]);
+}
+
+public int[] insertionSort(int[] x)
+ int n = array.length;  
+for (int j = 1; j < n; j++) {  
+int key = array[j];  
+int i = j-1;  
+ while ( (i > -1) && ( array [i] > key ) ){  
+array [i+1] = array [i];  
+i--;  
+}  
+array[i+1] = key;  
+}  
+public int List<Integer> selectionSort(List<Integer> x)
+for(int i = 0; i < a.length-1; i++){
+int maxPosition=i;
+int minPosition=i;
+for(int j = i+1; j < a.length; j++){
+if(a[j] < a[minPosition]){
+minPosition = j;
+}
+if(a[j] > a[maxPosition])
+maxPosition = j;
+}
+}
+swap(a,maxPosition,i);
+swap(a,minPosition,a.length-i-1);
+}
+System.out.println();
+}
+public static void swap(int[]a, int i, int j){
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+public int List<Integer> insertionSort(List<Integer> x)
+}  
--- src/main/java/u7pp/War.java
+++ src/main/java/u7pp/War.java
@@ -1,6 +1,13 @@
 package u7pp;
+import.java.util.Scanner;
 
 
 public class War {
-    
+private string player1 = new Scanner(System.in); 
+private string player2 = new Scanner(System.in);
+int player1 = mycard
+int player2 = mycard
+   
+War game = new War();
+game.play();
 }
